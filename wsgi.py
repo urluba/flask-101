232a02d8c89b0cdf2b6d00d15eb586dfa7eca2a4
urluba <lucas.barriere@gmail.com>
2018-07-11 11:01:43 +0200

    Added tests skel

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/api/v1/products')
+def products():
+    the_products = [
+        { 'id': 1, 'name': 'Skello' },
+        { 'id': 2, 'name': 'Socialive.tv' },
+        { 'id': 3, 'name': 'Truc' },
+        { 'id': 4, 'name': 'Bidule' },
+    ]
+
+    return jsonify(the_products)
+

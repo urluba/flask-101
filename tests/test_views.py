23b470f946200bca8120f6272d257e8da568fb4e
urluba <lucas.barriere@gmail.com>
2018-07-11 11:55:21 +0200

    Added DELETE Refacto the_products

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,11 +1,33 @@
 # tests/test_views.py
 from flask_testing import TestCase
-from wsgi import app
+from wsgi import app, ProductDb
 
 class TestViews(TestCase):
+    # def setUp(self):
+
+    #     the_products = ProductDb(
+    #         [
+    #             { 'id': 1, 'name': 'Skello' },
+    #             { 'id': 2, 'name': 'Socialive.tv' },
+    #             { 'id': 3, 'name': 'Truc' },
+    #             { 'id': 4, 'name': 'Bidule' },
+    #         ]
+    #     )
+
     def create_app(self):
         app.config['TESTING'] = True
         return app
+
+    def test_delete_missing_product(self):
+        response = self.client.delete("/api/v1/products/666")
+        self.assertEquals(response.status_code, 404)
+
+    def test_delete_product(self):
+        response = self.client.delete("/api/v1/products/4")
+        self.assertEquals(response.status_code, 204)
+
+        response = self.client.get("/api/v1/products/4")
+        self.assertEquals(response.status_code, 404)
 
     def test_get_existing_product_by_id(self):
         response = self.client.get("/api/v1/products/1")

e39a737ff6713e7d24afdc8a7de53f5d789b4780
urluba <lucas.barriere@gmail.com>
2018-07-11 11:11:57 +0200

    Added GET method on product

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,25 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import logging
+
 app = Flask(__name__)
 
+the_products = [
+    { 'id': 1, 'name': 'Skello' },
+    { 'id': 2, 'name': 'Socialive.tv' },
+    { 'id': 3, 'name': 'Truc' },
+    { 'id': 4, 'name': 'Bidule' },
+]
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+            'status': 404,
+            'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
 
 @app.route('/')
 def hello():
@@ -8,12 +27,15 @@ def hello():
 
 @app.route('/api/v1/products')
 def products():
-    the_products = [
-        { 'id': 1, 'name': 'Skello' },
-        { 'id': 2, 'name': 'Socialive.tv' },
-        { 'id': 3, 'name': 'Truc' },
-        { 'id': 4, 'name': 'Bidule' },
-    ]
-
     return jsonify(the_products)
 
+@app.route('/api/v1/products/<productid>')
+def get_productid(productid: str):
+    productid = int(productid)
+
+    for product in the_products:
+        if product['id'] == productid:
+            logging.debug('Found product %s: "%s"', productid, product)
+            return jsonify(product)
+
+    return not_found(error='Product id not found')
